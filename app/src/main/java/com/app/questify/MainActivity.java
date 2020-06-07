d9fb52c9d219d90530fb4f616eca59d5e52bb2e0
Arpchan LLC <arpchanllc@gmail.com>
2020-06-07 10:53:02 -0700

    Initial Commint

---
+++ app/src/main/java/com/app/questify/MainActivity.java
@@ -0,0 +1,26 @@
+package com.app.questify;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+    private EditText name;
+    private EditText passWord;
+    private TextView info;
+    private Button login;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        name = (EditText) findViewById(R.id.etName);
+        passWord = (EditText) findViewById(R.id.eTPassword);
+        info = (TextView) findViewById(R.id.tvInfo);
+        login = (Button) findViewById(R.id.btnLogin);
+    }
+
+}
