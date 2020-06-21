25d11ebcdc4c4ac8c3bc80edda1a432d03a36f26
appquestify <appquestify@gmail.com>
2020-06-20 23:30:09 -0700

    make the page reachable

--- app/src/main/java/com/app/questify/MainPage.java
+++ app/src/main/java/com/app/questify/MainPage.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ImageView;
 
 public class MainPage extends AppCompatActivity {
 
@@ -12,10 +13,20 @@ public class MainPage extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_page);
+        ImageView all_question = (ImageView) findViewById(R.id.imgAllQuestion);
+        all_question.setClickable(true);
+        all_question.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent myIntent = new Intent(getBaseContext(),   AllQuestions.class);
+                startActivity(myIntent);
+            }
+        });
     }
 
     public void onButtonClick(View v){
         Intent myIntent = new Intent(getBaseContext(),   AllQuestions.class);
         startActivity(myIntent);
     }
+
 }
